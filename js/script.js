bb332d223d9eb1e108a6ec63e354377bc2ee9757
PSKutergin <PSKutergin@gmail.com>
2023-08-31 12:26:28 +0300

    create creating variables

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const title = 'first lesson'
+const screens = 10
+const screenPrice = 15000
+const rollback = 0.1
+const fullPrice = 238892
+const adaptive = true
+
+if (adaptive) {
+  alert('I completed the ' + title)
+}
