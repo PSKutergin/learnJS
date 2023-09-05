61078ede5ec171ddc9c9e8d80a0d57e079aafb9b
PSKutergin <PSKutergin@gmail.com>
2023-09-05 23:28:04 +0300

    functions and callback

--- js/script.js
+++ js/script.js
@@ -1,22 +1,18 @@
 'use strict'
 
 function main() {
-  const title = prompt('Как называется ваш проект?')
-  const screens = prompt(
+  let title = prompt('Как называется ваш проект?')
+  let screens = prompt(
     'Какие типы экранов нужно разработать?',
     'Простые, Сложные, Интерактивные',
   )
-  const screenPrice = +prompt('Сколько будет стоить данная работа?')
-  const rollback = 20
-  const adaptive = confirm('Нужен ли адаптив на сайте?')
-  const service1 = prompt('Какой дополнительный тип услуги нужен?')
-  const servicePrice1 = +prompt('Сколько это будет стоить?')
-  const service2 = prompt('Какой дополнительный тип услуги нужен?')
-  const servicePrice2 = +prompt('Сколько это будет стоить?')
-  const fullPrice = screenPrice + servicePrice1 + servicePrice2
-  const servicePercentPrice = Math.ceil(
-    fullPrice - fullPrice * (rollback / 100),
-  )
+  let screenPrice = +prompt('Сколько будет стоить данная работа?')
+  let rollback = 20
+  let adaptive = confirm('Нужен ли адаптив на сайте?')
+  let service1 = prompt('Какой дополнительный тип услуги нужен?')
+  let servicePrice1 = +prompt('Сколько это будет стоить?')
+  let service2 = prompt('Какой дополнительный тип услуги нужен?')
+  let servicePrice2 = +prompt('Сколько это будет стоить?')
 
   const showTypeOF = function (variable) {
     console.log(variable, typeof variable)
@@ -34,39 +30,34 @@ function main() {
     }
   }
 
+  const getAllServicePrices = function (price1, price2) {
+    return price1 + price2
+  }
+  function getFullPrice(price, callback) {
+    return price + callback
+  }
+  const getServicePercentPrices = function (price, rollback) {
+    return Math.ceil(price - price * (rollback / 100))
+  }
+  const getTitle = function (str) {
+    return (
+      str.trim().charAt(0).toUpperCase() + str.trim().slice(1).toLowerCase()
+    )
+  }
+
+  let fullPrice = getFullPrice(
+    screenPrice,
+    getAllServicePrices(servicePrice1, servicePrice2),
+  )
+  let servicePercentPrice = getServicePercentPrices(fullPrice, rollback)
+  title = getTitle(title)
+
   showTypeOF(title)
   showTypeOF(fullPrice)
   showTypeOF(adaptive)
 
-  console.log(getRollbackMessage(fullPrice))
-  console.log(typeof title)
-  console.log(typeof fullPrice)
-  console.log(typeof adaptive)
-
-  console.log(screens.length)
-  console.log('Стоимость верстки экранов ' + screenPrice + ' рублей')
-  console.log('Стоимость разработки сайта ' + fullPrice + ' рублей')
-  console.log(
-    'Стоимость верстки экранов ' + Math.round(screenPrice / 96.2) + ' долларов',
-  )
-  console.log(
-    'Стоимость разработки сайта ' + Math.round(fullPrice / 96.2) + ' долларов',
-  )
-  console.log(
-    'Стоимость верстки экранов ' + Math.round(screenPrice * 0.39) + ' гривен',
-  )
-  console.log(
-    'Стоимость разработки сайта ' + Math.round(fullPrice * 0.39) + ' гривен',
-  )
-  console.log(
-    'Стоимость верстки экранов ' + Math.round(screenPrice / 13.2) + ' юани',
-  )
-  console.log(
-    'Стоимость разработки сайта ' + Math.round(fullPrice / 13.2) + ' юани',
-  )
   console.log(screens.toLowerCase().split(', '))
-  console.log(fullPrice * (rollback / 100))
-
+  console.log(getRollbackMessage(fullPrice))
   console.log(
     'Стоимость за вычетом отката посреднику ' + servicePercentPrice + ' рублей',
   )
